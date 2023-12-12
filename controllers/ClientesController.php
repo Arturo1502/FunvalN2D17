cdf71517e37af4093fffe4a2a7c836124ebb2292
Arturo1502 <149508454+Arturo1502@users.noreply.github.com>
2023-12-12 09:43:57 -0500

    first commit

---
+++ controllers/ClientesController.php
@@ -0,0 +1,16 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/Models/Clientes.php';
+
+
+
+class ClientesController{
+
+    public function index(){
+        $clientes = new Clientes();
+        $clientes->All();
+        return $clientes;
+    }
+}
+
+
+?>
